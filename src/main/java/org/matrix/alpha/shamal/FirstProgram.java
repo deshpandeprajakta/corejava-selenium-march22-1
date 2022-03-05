83caed0735672abb2fba1a49b38d417f09858eab
shamal18 <jadhavshamali1992>
2022-03-05 23:06:02 +0530

    add the sysytem out pritln ststment

---
+++ src/main/java/org/matrix/alpha/shamal/FirstProgram.java
@@ -0,0 +1,9 @@
+package org.matrix.alpha.shamal;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("This is the the my first try!");
+        System.out.println("This is the github trial");
+    }
+}
