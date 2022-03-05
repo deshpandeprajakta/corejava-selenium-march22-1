b0ee17dfd904d5be4f0a5faf209546d8ecf9e345
shamal18 <jadhavshamali1992>
2022-03-05 23:33:24 +0530

    First addition program trial

--- src/main/java/org/matrix/alpha/shamal/FirstProgram.java
+++ src/main/java/org/matrix/alpha/shamal/FirstProgram.java
@@ -3,8 +3,11 @@ package org.matrix.alpha.shamal;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("This is the the my first try!");
-        System.out.println("This is the github trial");
-        System.out.println("This is the github push and pull trial");
+        System.out.println("This is the my first try!");
+        int num1 = 20; int num2=30; int sum ;
+        sum = num1 + num2;
+
+        System.out.println("addition will be:"+sum);
+
     }
 }
