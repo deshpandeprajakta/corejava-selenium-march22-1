78254de239b92a7ab9f551dd36d72bddd4dc2fe8
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-05 21:29:57 +0530

    Created new file for Intellij demo

---
+++ src/main/java/org/matrix/shriniwas/corejava/HelloTestng.java
@@ -0,0 +1,9 @@
+package org.matrix.shriniwas.corejava;
+
+public class HelloTestng {
+
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("My age is:" +age);
+    }
+}
