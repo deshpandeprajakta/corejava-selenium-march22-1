5adc2b3beed93bf7d71adef1e78fffa032a276dc
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-05 21:18:31 +0530

    Deleted some code

--- src/main/java/org/matrix/shriniwas/corejava/HelloSelenium.java
+++ src/main/java/org/matrix/shriniwas/corejava/HelloSelenium.java
@@ -2,6 +2,6 @@ package org.matrix.shriniwas.corejava;
 
 public class HelloSelenium {
     public static void main(String[] args) {
-        System.out.println("Updating code");
+        System.out.println("Updating");
     }
 }
