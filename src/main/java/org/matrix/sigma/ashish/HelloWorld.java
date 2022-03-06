a47394b98f566d5349ada1e3e384c8951b3d0c4b
Ashishthopate <ashishthopte27@gmail.com>
2022-03-06 21:52:03 +0530

    added sout statement

--- src/main/java/org/matrix/sigma/ashish/HelloWorld.java
+++ src/main/java/org/matrix/sigma/ashish/HelloWorld.java
@@ -2,7 +2,8 @@ package org.matrix.sigma.ashish;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, Welcome to Java!");
+    public static void main(String[] args) { System.out.println("Hello, Welcome to Java!");
+    System.out.println("Hello I am Ashish");
+        System.out.println("Ashish");
     }
 }
