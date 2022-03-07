d162f95a44466a9b0083ee52f4e0b4df0639b0ae
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-07 15:11:18 +0530

    Added new file

---
+++ src/main/java/org/matrix/omega/samir/Calculations.java
@@ -0,0 +1,24 @@
+package org.matrix.omega.samir;
+import java.util.*;
+public class Calculations {
+
+    void addition(int i,int y){
+
+        int z=i+y;
+        System.out.println("Addition of two number "+ z);
+    }
+    void subtraction(int i,int y){
+        int z=i-y;
+        System.out.println("Subtraction of two numbers "+ z);
+    }
+    public static void main(String[] args) {
+        Calculations cal= new Calculations();
+        Scanner  sc = new Scanner(System.in);
+        int i= sc.nextInt();
+        int y=sc.nextInt();
+        cal.addition( i,y);
+        cal.subtraction(i,y);
+
+
+    }
+}
