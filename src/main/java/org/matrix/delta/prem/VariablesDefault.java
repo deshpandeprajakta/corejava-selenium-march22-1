cb833cbe18f0229cfc5ef40bbd72b56230eefd6c
Premp15 <prem74729@gmail.com>
2022-03-08 02:18:11 +0530

    Default values of data types

---
+++ src/main/java/org/matrix/delta/prem/VariablesDefault.java
@@ -0,0 +1,20 @@
+package org.matrix.delta.prem;
+
+public class VariablesDefault{
+
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean b;
+    static String str;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(str);
+    }
+}
