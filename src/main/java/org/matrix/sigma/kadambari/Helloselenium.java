44a571eb9599b5dad25954757d8af3a2d4293d90
kadambari <kadambari.belavate@gmail.com>
2022-03-07 02:06:39 -0800

    Added sout statement

--- src/main/java/org/matrix/sigma/kadambari/Helloselenium.java
+++ src/main/java/org/matrix/sigma/kadambari/Helloselenium.java
@@ -1,7 +1,9 @@
 package org.matrix.sigma.kadambari;
 
 public class Helloselenium {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
     System.out.println("Software Testing Class");
+        System.out.println("Hello selenium");
     }
     }
