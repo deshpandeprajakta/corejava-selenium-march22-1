912ff299911bf451b2256b43282bcb8a995d209a
Premp15 <prem74729@gmail.com>
2022-03-08 01:46:38 +0530

    "Table Program"

---
+++ src/main/java/org/matrix/delta/prem/TableProgram.java
@@ -0,0 +1,11 @@
+package org.matrix.delta.prem;
+
+public class TableProgram {
+    public static void main(String[] args) {
+
+        int num=15;
+        for(int i=1;i<=10;i++){
+            System.out.println("15 * "+i+" = "+i*num);
+        }
+    }
+}
