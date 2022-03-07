1e8ffb18b24a967e6e85010c91eac9b3d1f1865c
Ashishthopate <ashishthopte27@gmail.com>
2022-03-07 20:06:09 +0530

    creted new file for commit demo

---
+++ src/main/java/org/matrix/sigma/ashish/HelloCommit.java
@@ -0,0 +1,8 @@
+package org.matrix.sigma.ashish;
+
+public class HelloCommit {
+    public static void main(String[] args) {
+        int age=30;
+        System.out.println("My age is " +age);
+    }
+}
