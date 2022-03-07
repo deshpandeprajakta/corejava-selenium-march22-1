ebcd7d6c49403430a3ca9757dc3f3e2fcb836ca4
Premp15 <prem74729@gmail.com>
2022-03-08 01:41:41 +0530

    "IfElse and Table Program"

---
+++ src/main/java/org/matrix/delta/prem/IfELsePractice.java
@@ -0,0 +1,16 @@
+package org.matrix.delta.prem;
+
+public class IfELsePractice {
+    public static void main(String[] args) {
+        int a=45,b=50;
+        if(a>b){
+            System.out.println("A is greater");
+        }else{
+            System.out.println("B is greater");
+        }
+        int num=15;
+        for(int i=1;i<=10;i++){
+            System.out.println("15 *"+i+"="+i*num);
+        }
+    }
+}
