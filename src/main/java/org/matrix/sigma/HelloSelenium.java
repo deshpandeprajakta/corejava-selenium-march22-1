57f2075ca3c5549274227f626389512fbcb2c044
Ashishthopate <ashishthopte27@gmail.com>
2022-03-07 08:53:19 +0530

    updated code

--- src/main/java/org/matrix/sigma/HelloSelenium.java
+++ src/main/java/org/matrix/sigma/HelloSelenium.java
@@ -1,5 +1,9 @@
 package org.matrix.sigma;
 
 public class HelloSelenium {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello Selenium");
+        System.out.println("Updating code");
+    }
+
 }
