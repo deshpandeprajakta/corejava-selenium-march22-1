d47037e561156419abcfcaba32a1f4d8a0741360
Ashishthopate <ashishthopte27@gmail.com>
2022-03-07 20:09:09 +0530

    added some code

--- src/main/java/org/matrix/sigma/ashish/HelloCommit.java
+++ src/main/java/org/matrix/sigma/ashish/HelloCommit.java
@@ -4,5 +4,6 @@ public class HelloCommit {
     public static void main(String[] args) {
         int age=30;
         System.out.println("My age is " +age);
+        System.out.println("Hi , I am Ashish");
     }
 }
