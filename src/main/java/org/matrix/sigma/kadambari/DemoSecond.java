b50d79e965b97f5291085e5a53e42048d385639e
kadambari <kadambari.belavate@gmail.com>
2022-03-07 02:26:41 -0800

    Added new file

---
+++ src/main/java/org/matrix/sigma/kadambari/DemoSecond.java
@@ -0,0 +1,11 @@
+package org.matrix.sigma.kadambari;
+
+public class DemoSecond {
+    public static void main(String[] args)
+    {
+     int i =10;
+     int s=20;
+     int j= i+s;
+        System.out.println("Addition of two number" +j);
+    }
+}
