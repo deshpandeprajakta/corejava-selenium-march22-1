bd458c09a0f0cf4e3d452619cd1eeb2b330665c2
nayan jadhav <nayan100.nj@gmail.com>
2022-03-08 14:12:30 +0530

    "added"

---
+++ src/main/java/org/matrix/omega/nayan/ClassValue.java
@@ -0,0 +1,24 @@
+package org.matrix.omega.nayan;
+
+class Employee {
+
+
+}
+
+class Student {
+
+
+}
+
+public class ClassValue {
+
+    static Employee nn;
+    static Student rr;
+
+
+    public static void main(String[] args) {
+        System.out.println("class Employee bydefault value :" + nn);
+        System.out.println("class Student bydefault value :" + rr);
+    }
+
+}
