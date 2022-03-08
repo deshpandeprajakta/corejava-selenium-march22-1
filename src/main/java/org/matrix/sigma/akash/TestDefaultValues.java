25b96b19ab0ddd1d08222d2240036a8b09de7964
Akash-0103 <akash.kudalkar001@gmail.com>
2022-03-08 19:04:53 +0530

    Program for Default value

---
+++ src/main/java/org/matrix/sigma/akash/TestDefaultValues.java
@@ -0,0 +1,26 @@
+package org.matrix.sigma.akash;
+/* Note - To get the by default values of variable must be static and static variable assigned
+outside the main method. */
+class Employee {
+}
+public class TestDefaultValues {
+    static int i;
+    static boolean a;
+    static long c;
+    static byte d;
+    static double e;
+    static short f;
+    static String g;
+    static Employee ee;
+    
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(ee);
+    }
+}
