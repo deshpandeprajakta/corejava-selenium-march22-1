a0314f43707317ddc8beaf1274b36749806bb53d
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-09 16:28:11 +0530

    Modified code - Ganesh

--- src/main/java/org/matrix/delta/ganesh/HelloSelenium.java
+++ src/main/java/org/matrix/delta/ganesh/HelloSelenium.java
@@ -6,7 +6,7 @@ public class HelloSelenium {
         System.out.println("Added code");
         System.out.println("Made changes on Git.. Now need to pull to local directory");
 
-        int a=10;
+        int a=100;
         System.out.println("value of a: "+a);
     }
 }
