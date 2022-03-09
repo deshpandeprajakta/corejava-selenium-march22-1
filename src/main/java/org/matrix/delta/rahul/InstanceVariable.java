36e6bd40f31c2efd0898fe316ace585c452394a8
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-09 22:23:15 +0530

    Methods concept

--- src/main/java/org/matrix/delta/rahul/InstanceVariable.java
+++ src/main/java/org/matrix/delta/rahul/InstanceVariable.java
@@ -1,19 +1,20 @@
 package org.matrix.delta.rahul;
 
 public class InstanceVariable {
-    int a=15;
-    int b=25;
-    InstanceVariable(){
-        System.out.println(a);
+    int a = 15;
+    int b = 25;
 
+    InstanceVariable() {
+        System.out.println(a);
     }
-    public void display(){
+
+    public void display() {
         System.out.println(b);
     }
 
     public static void main(String[] args) {
-        InstanceVariable e=new InstanceVariable();
+        InstanceVariable e = new InstanceVariable();
         //System.out.println(a);
-        e.dis();
+        e.display();
     }
 }
