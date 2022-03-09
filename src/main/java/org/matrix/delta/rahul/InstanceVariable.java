1b331e801d2d9a38f3fb0fbcc80515f434a1db71
Rahul S <shendagerb100@gmail.com>
2022-03-09 14:17:19 +0530

    modify in InstanceVariable

--- src/main/java/org/matrix/delta/rahul/InstanceVariable.java
+++ src/main/java/org/matrix/delta/rahul/InstanceVariable.java
@@ -3,8 +3,10 @@ package org.matrix.delta.rahul;
 public class InstanceVariable {
     int a=15;
     int b=25;
+    int c=50;
     InstanceVariable(){
         System.out.println(a);
+        System.out.println(c);
 
     }
     public void display(){
