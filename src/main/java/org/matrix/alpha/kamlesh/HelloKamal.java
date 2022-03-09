6e64d64710230d18899494e2d01d61001a22341a
Kamlesh Dandi <dandi8626@gmail.com>
2022-03-09 18:25:05 +0530

    Addded sout statement

--- src/main/java/org/matrix/alpha/kamlesh/HelloKamal.java
+++ src/main/java/org/matrix/alpha/kamlesh/HelloKamal.java
@@ -1,7 +1,7 @@
 package org.matrix.alpha.kamlesh;
 
-public class HelloWorld {
+public class HelloKamal {
     public static void main(String[] args) {
-        System.out.println("name_is_kamal");
+        System.out.println("Welcome to automation testing");
     }
 }
