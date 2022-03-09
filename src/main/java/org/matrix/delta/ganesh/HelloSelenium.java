39928e0f3b166e9157421f148e7f1aadaeb998e9
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-09 12:15:52 +0530

    Made changes on Git - Ganesh

--- src/main/java/org/matrix/delta/ganesh/HelloSelenium.java
+++ src/main/java/org/matrix/delta/ganesh/HelloSelenium.java
@@ -4,5 +4,6 @@ public class HelloSelenium {
     public static void main(String[] args) {
         System.out.println("In HelloSelenium class - Modify Code");
         System.out.println("Added code");
+        System.out.println("Made changes on Git.. Now need to pull to working directory");
     }
 }
