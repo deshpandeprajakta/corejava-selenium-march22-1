d84c09cdb0b966b7ad1b28e666e8d6a46a343579
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-10 21:47:57 +0530

    Update HelloJava.java

--- src/main/java/org/matrix/alpha/kamlesh/HelloJava.java
+++ src/main/java/org/matrix/alpha/kamlesh/HelloJava.java
@@ -2,6 +2,6 @@ package org.matrix.alpha.kamlesh;
 
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello name_is_kamal");
+        System.out.println("Hello name_is_kamal - Updated");
     }
 }
