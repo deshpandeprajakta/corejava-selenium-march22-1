ac09de6d2e2823215336e3ec6b5b30bb763b3792
97110106117 <anju.singam2711@gmail.com>
2022-03-10 16:42:30 +0530

    My first commit program

--- src/main/java/org/matrix/lambda/nisha/HelloTester.java
+++ src/main/java/org/matrix/lambda/nisha/HelloTester.java
@@ -3,8 +3,7 @@ package org.matrix.lambda.nisha;
 public class HelloTester {
 
     public static void main(String[] args) {
-        int n= 30;
-        System.out.println("Hello, Welcome to Java!");
-        System.out.println(n);
+        System.out.println("Hello, Welcome in the world of IT industry!");
+
     }
 }
