d0e44200d0345dc51b5a1322e9bb09954b460257
Monikashinde08 <Monikashinde08@gmail.com>
2022-03-10 11:02:08 -0800

    TestDefaultValues

---
+++ src/main/java/org/matrix/delta/monika/TestVariables.java
@@ -0,0 +1,14 @@
+package org.matrix.delta.monika;
+
+public class TestVariables {
+
+    void demo(){
+        int i = 30;
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println(age);
+    }
+}
