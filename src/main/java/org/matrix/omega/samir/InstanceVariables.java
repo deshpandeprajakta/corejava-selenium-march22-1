ca46fa6cf0fe6b3f557a218d0fd22a5e5aa3bb1e
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-10 18:01:30 +0530

    Added some code

--- src/main/java/org/matrix/omega/samir/InstanceVariables.java
+++ src/main/java/org/matrix/omega/samir/InstanceVariables.java
@@ -1,10 +1,28 @@
 package org.matrix.omega.samir;
 
+import java.util.Scanner;
+
 public class InstanceVariables {
-    int salary=5000;
+    int salary = 5000; // instance variable
+    static String companyName = "ABC"; // Static Variable
+
+    void checkEvenOrOdd(int num) { //non static method
+        if (num % 2 == 0) {
+            System.out.println("Given Number" + num + "is Even  Number");
+        } else {
+            System.out.println("Given Number" + num + "is Odd  Number");
+        }
+    }
 
     public static void main(String[] args) {
-        InstanceVariables obj=new InstanceVariables();
-        System.out.println(obj.salary);
+        InstanceVariables obj = new InstanceVariables();
+        System.out.println("SALARY IS " + obj.salary);//calling using object
+        System.out.println("Company Name is " + companyName);//direct calling because it is a static variable
+        System.out.println("Enter the value of num");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        obj.checkEvenOrOdd(num);//calling using object
+
+
     }
 }
