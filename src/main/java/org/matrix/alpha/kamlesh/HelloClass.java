f1bf2eae5d66026b80c82c47f2e5e200b99ee32f
Kamlesl Dandi <97212962+Kamaldandi@users.noreply.github.com>
2022-03-10 22:06:12 +0530

    Update HelloClass.java

--- src/main/java/org/matrix/alpha/kamlesh/HelloClass.java
+++ src/main/java/org/matrix/alpha/kamlesh/HelloClass.java
@@ -2,6 +2,7 @@ package org.matrix.alpha.kamlesh;
 
 public class HelloClass {
     public static void main(String[] args) {
+        System.out.println("test in progress");
         System.out.println("Created another 1");
     }
 }
