84a59fc7960aad06e780dc8057ca0483743338fd
97110106117 <anju.singam2711@gmail.com>
2022-03-10 16:58:17 +0530

    Comments in java

---
+++ src/main/java/org/matrix/lambda/nisha/JavaComments.java
@@ -0,0 +1,17 @@
+package org.matrix.lambda.nisha;
+
+// Simple examples of comments
+
+public class JavaComments {
+
+    //Your program begins with a call to main()
+    // this is single line comment
+
+    public static void main(String[] args) {
+        System.out.println("Java Programming Examples");
+    }
+
+    /*This is multiline comment
+    with multiple lines   */
+
+}
