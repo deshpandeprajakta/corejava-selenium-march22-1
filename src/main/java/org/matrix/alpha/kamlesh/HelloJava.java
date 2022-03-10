09b51230fc8bed2aa6efc8abb7b4b432247a5237
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-10 21:49:19 +0530

    - Merge conflict example

--- src/main/java/org/matrix/alpha/kamlesh/HelloJava.java
+++ src/main/java/org/matrix/alpha/kamlesh/HelloJava.java
@@ -3,5 +3,8 @@ package org.matrix.alpha.kamlesh;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello name_is_kamal");
+        System.out.println("This is statement-1");
+        System.out.println("This is statement-2");
+        System.out.println("This is statement-3");
     }
 }
