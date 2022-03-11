f64a2014a7f9ae1710dee8b72b2cd4747cd550a9
AkashNGupta <101316612+AkashNGupta@users.noreply.github.com>
2022-03-11 19:34:06 +0530

    Create HelloWorld1.java

---
+++ src/main/java/org/matrix/lambda/akashgupta/HelloWorld1.java
@@ -0,0 +1,8 @@
+package org.matrix.lambda.akashgupta;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("File added through (github) server");
+    }
+}
