ffa428b1800adc216519241c6649e78815d17b0f
AkashNGupta <akashngupta2116@gmail.com>
2022-03-11 14:34:28 +0530

    Added sout statement

--- src/main/java/org/matrix/lambda/akashgupta/HelloWorld.java
+++ src/main/java/org/matrix/lambda/akashgupta/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Java!");
-        System.out.println("Hello, Git repository");
+        System.out.println("Hello, Git Repository");
     }
 }
