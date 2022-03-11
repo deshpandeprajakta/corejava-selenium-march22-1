477e3d44e3de842e4450bb2aed52861537862535
AkashNGupta <akashngupta2116@gmail.com>
2022-03-11 19:29:22 +0530

    Merge Conflict example

--- src/main/java/org/matrix/lambda/akashgupta/HelloWorld.java
+++ src/main/java/org/matrix/lambda/akashgupta/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Java!");
-        System.out.println("Change through (github)gitserver");
+        System.out.println("local change");
     }
 }
