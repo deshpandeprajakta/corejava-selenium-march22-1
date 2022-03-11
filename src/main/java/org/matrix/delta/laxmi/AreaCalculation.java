8682a6d2cfeb7477f8ec81bf00d01eea122a9e11
laxmikurhade <laxmikurhade09@gmail.com>
2022-03-11 21:41:06 +0530

    Added new file of AreaCalculation

---
+++ src/main/java/org/matrix/delta/laxmi/AreaCalculation.java
@@ -0,0 +1,8 @@
+package org.matrix.delta.laxmi;
+
+public class AreaCalculation {
+       public static void main(String[] args) {
+        int squareSide = 12;
+        System.out.println("Area of Square:" + (squareSide * squareSide));
+    }
+}
