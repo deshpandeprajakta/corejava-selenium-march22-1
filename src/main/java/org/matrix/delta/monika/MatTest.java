58c5820a2551d1d44b858357a5b5022f15672002
Monikashinde08 <Monikashinde08@gmail.com>
2022-03-10 22:53:08 -0800

    added sout statement

---
+++ src/main/java/org/matrix/delta/monika/MatTest.java
@@ -0,0 +1,15 @@
+package org.matrix.delta.monika;
+
+public class MatTest {
+    public static int studId = 100;
+    public static String str = "coem";
+    public static char c = 'A';
+
+    public static void main(String[] args) {
+        System.out.println(studId);
+        System.out.println(str);
+        System.out.println(c);
+    }
+    }
+
+
