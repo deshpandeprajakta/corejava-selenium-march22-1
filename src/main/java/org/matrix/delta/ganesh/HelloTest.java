8448ff0d60dcffec6b428163d6001dc5bddfd8ff
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-11 16:20:23 +0530

    Test Merge Conflict concept

--- src/main/java/org/matrix/delta/ganesh/HelloTest.java
+++ src/main/java/org/matrix/delta/ganesh/HelloTest.java
@@ -2,6 +2,6 @@ package org.matrix.delta.ganesh;
 
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("New Java File");
+        System.out.println("I am Shrinivas making changes on same line for testing Merge Conflict");
     }
 }
