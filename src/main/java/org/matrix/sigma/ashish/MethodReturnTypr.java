15cb8f6f7ed5c265d72645b655210f756169f743
Ashishthopate <ashishthopte27@gmail.com>
2022-03-12 22:49:57 +0530

    Return Program

--- src/main/java/org/matrix/sigma/ashish/MethodReturnTypr.java
+++ src/main/java/org/matrix/sigma/ashish/MethodReturnTypr.java
@@ -12,12 +12,17 @@ public class MethodReturnTypr {
         return c;//return value
 
     }
+    public String GetName(String name){
+        return name;
+    }
 
     public static void main(String[] args) {
         MethodReturnTypr obj=new MethodReturnTypr();
 
         int c = obj.m1(10,20);//take value in c
        int d = obj.m2(10,20);//take value in d
+        System.out.println("name   "+obj.GetName("Ashish"));
+
 
         System.out.println("addition  "+c);
         System.out.println(("Multiplication  "+d));
