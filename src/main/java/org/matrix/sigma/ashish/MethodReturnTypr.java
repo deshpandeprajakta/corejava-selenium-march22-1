d35e4bbe5c017c91acf3c412e0509140895cbabd
Ashishthopate <ashishthopte27@gmail.com>
2022-03-12 22:41:50 +0530

    Return Program

---
+++ src/main/java/org/matrix/sigma/ashish/MethodReturnTypr.java
@@ -0,0 +1,25 @@
+package org.matrix.sigma.ashish;
+
+public class MethodReturnTypr {
+
+    public int m1(int a,int b){
+        int c =a+b;
+        return c;//return value
+    }
+
+    public int m2(int a,int b){
+        int c =a*b;
+        return c;//return value
+
+    }
+
+    public static void main(String[] args) {
+        MethodReturnTypr obj=new MethodReturnTypr();
+
+        int c = obj.m1(10,20);//take value in c
+       int d = obj.m2(10,20);//take value in d
+
+        System.out.println("addition  "+c);
+        System.out.println(("Multiplication  "+d));
+    }
+}
