d3e7ebeea847458c5b834c1631b3e7f98723a63f
Rahul S <shendagerb100@gmail.com>
2022-03-13 15:23:38 +0530

    Method Callings Program

---
+++ src/main/java/org/matrix/delta/rahul/MethodCalling.java
@@ -0,0 +1,24 @@
+package org.matrix.delta.rahul;
+
+public class MethodCalling {
+    public void m1() {
+        System.out.println("i am in method m1");
+        m2();  // called the m2 method in m1 method.
+    }
+
+    public void m2() {
+        System.out.println("I am in Method m2");
+
+    }
+
+    public void m3() {
+        System.out.println("I am in method m3");
+    }
+
+    public static void main(String[] args) {
+        MethodCalling obj = new MethodCalling();
+        obj.m1(); //called the m1 method.
+        // obj.m2();
+        // obj.m2();
+    }
+}
