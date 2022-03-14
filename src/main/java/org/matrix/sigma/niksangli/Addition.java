5487ba086cfacf3c25528216dfac27216fbdaaba
Nikhilcoolka <Niknov1990>
2022-03-14 18:15:22 +0530

    Added addition prgram

---
+++ src/main/java/org/matrix/sigma/niksangli/Addition.java
@@ -0,0 +1,10 @@
+package org.matrix.sigma.niksangli;
+
+public class Addition {
+    public static void main(String[] args)
+    {
+      int a=20; int b=30;
+      int c=a+b;
+        System.out.println("Addition of number is" +c);
+    }
+}
