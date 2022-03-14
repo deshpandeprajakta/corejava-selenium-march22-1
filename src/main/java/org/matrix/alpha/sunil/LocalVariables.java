819952d9c2fb055e7eef4e6b404ab03caa9335b2
Sunil <sunilsonavane88@gmail.com>
2022-03-14 19:28:56 +0530

    Local Variable

---
+++ src/main/java/org/matrix/alpha/sunil/LocalVariables.java
@@ -0,0 +1,9 @@
+package org.matrix.alpha.sunil;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+        int age = 10;
+        System.out.println(age);
+        System.out.println("Variable Declaration under Methods");
+    }
+}
