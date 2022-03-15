d8ca40abd81adc81c3526500318ef3f6001845fb
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-15 16:03:16 +0530

    Method Details - Ganesh

---
+++ src/main/java/org/matrix/delta/ganesh/MethodsAccess.java
@@ -0,0 +1,20 @@
+package org.matrix.delta.ganesh;
+
+public class MethodsAccess {
+
+    public void college(){
+        String empName = "Raj";
+        int empID = 101;
+        System.out.println("Methods Access: " +empName);
+        System.out.println("Methods Access: " +empID);
+
+    }
+
+    public void university(){
+        String name = "BOMBAY";
+        System.out.println("Methods Access: " +name);
+
+    }
+
+
+}
