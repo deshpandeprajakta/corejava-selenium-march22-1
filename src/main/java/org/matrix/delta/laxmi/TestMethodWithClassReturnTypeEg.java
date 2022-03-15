e70ea017ca95bf6dc9a3a0c36c500acc0595cccf
laxmikurhade <laxmikurhade09@gmail.com>
2022-03-15 16:42:07 +0530

    Method With Class ReturnType program added

---
+++ src/main/java/org/matrix/delta/laxmi/TestMethodWithClassReturnTypeEg.java
@@ -0,0 +1,14 @@
+package org.matrix.delta.laxmi;
+
+public class TestMethodWithClassReturnTypeEg {
+
+    public static AreaCalculation getCalculatedArea() {
+        return new AreaCalculation();
+    }
+
+    public static void main(String[] args) {
+        AreaCalculation obj1=getCalculatedArea();
+       int a= obj1.areaOfRectangle(10,20);
+        System.out.println("Calculated area of rectangle using function return type is:"+a);
+    }
+}
