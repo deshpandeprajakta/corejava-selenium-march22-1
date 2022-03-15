c116aeb70292dc54b9247e12f35491fb93b0a5ff
amruta95327 <patilamruta16@gmail.com>
2022-03-15 13:04:03 +0530

    addition of two numbers

--- src/main/java/org/matrix/lambda/amruta/TestLocalVariables.java
+++ src/main/java/org/matrix/lambda/amruta/TestLocalVariables.java
@@ -1,9 +1,15 @@
 package org.matrix.lambda.amruta;
 
 public class TestLocalVariables {
+
     public static void main(String[] args) {
         int age=30;
         System.out.println(age);
+    }
 
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Addition of two numbers");
     }
 }
