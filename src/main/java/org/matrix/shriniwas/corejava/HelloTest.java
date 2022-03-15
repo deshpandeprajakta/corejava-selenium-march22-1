5358ca9b97e47a46a4f730226c19d61234e3e5e7
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-15 22:48:07 +0530

    - Added new sentence

--- src/main/java/org/matrix/shriniwas/corejava/HelloTest.java
+++ src/main/java/org/matrix/shriniwas/corejava/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello, Shriniwas!");
         System.out.println("We are learning branching concept.");
+        System.out.println("New Sentence");
     }
 }
