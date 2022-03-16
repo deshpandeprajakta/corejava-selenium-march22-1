bdc99ccc853458448f5dbead2bb9cd99bed328f5
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-16 15:51:24 +0530

    Added extra statement from local branch - MAT-05-GaneshP

--- src/main/java/org/matrix/delta/ganesh/HelloTest.java
+++ src/main/java/org/matrix/delta/ganesh/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("I am Shrinivas making changes on same line for testing Merge Conflict");
         System.out.println("This is to test Github Conflit and Merge Concept");
+        System.out.println("This is first statement after creating new Branch on local : MAT-05-GaneshP");
     }
 }
