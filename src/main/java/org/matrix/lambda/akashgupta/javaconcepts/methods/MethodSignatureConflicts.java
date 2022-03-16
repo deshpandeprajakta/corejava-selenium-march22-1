b5b51508cb53598997718abd6a967f2096518bf1
AkashNGupta <akashngupta2116@gmail.com>
2022-03-16 17:43:48 +0530

    signature conflict

---
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/methods/MethodSignatureConflicts.java
@@ -0,0 +1,25 @@
+package org.matrix.lambda.akashgupta.javaconcepts.methods;
+/* .........Same method name creates conflicts with same class
+.......can resolve by only different type of argument
+ .........same arguments are also not allowed
+ .......... but you can rearrange them
+ ....... concept of polymorphism      */
+
+public class MethodSignatureConflicts {
+    public void sameName() {
+        System.out.println("method without parameters");
+    }
+
+    //private void sameName(){} // doesn't care for access specifier
+    //public int sameName(){} // doesn't care for return type
+    private void sameName(int a) {
+    }
+
+    public void sameName(int a, String str) {
+    }
+
+    public String sameName(String str, int a) {
+        return str;
+    }
+
+}
