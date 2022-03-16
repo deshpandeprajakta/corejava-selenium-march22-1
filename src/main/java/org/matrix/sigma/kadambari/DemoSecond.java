31c46ad291637011eb65e829066876fb56a365f9
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-16 20:05:03 +0530

    Added some code

--- src/main/java/org/matrix/sigma/kadambari/DemoSecond.java
+++ src/main/java/org/matrix/sigma/kadambari/DemoSecond.java
@@ -1,12 +1,13 @@
-package org.matrix.sigma.kadambari;
+/*package org.matrix.sigma.kadambari;
 
 public class DemoSecond {
-    public static void main(String[] args) {
-        int i = 10;
-        int s = 20;
-        int j = i + s;
-        int z = i - s;
-        System.out.println("Addition of two number" + j);
-        System.out.println("substraction of two number" + j);
+    public static void main(String[] args)
+    {
+     int i =10;
+     int s=20;
+     int j= i+s;
+     int z=i-s;
+        System.out.println("Addition of two number" +j);
+        System.out.println("substraction of two number"+j);
     }
-}
+}*/
