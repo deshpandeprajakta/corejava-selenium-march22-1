80ab1dbb7b0ace44aa5170b112bef806de253c92
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-16 20:16:26 +0530

    Merge branch 'master' of https://github.com/shriniwasalle/corejava-selenium-march22

---
+++ src/main/java/org/matrix/sigma/pankaj/TestLocalVariable.java
@@ -0,0 +1,32 @@
+package org.matrix.sigma.pankaj;
+
+public class TestLocalVariable {
+
+  /*  {
+          int age2 = 24;   this is called blocks
+      }  */
+
+    /*   TestLocalVariable() {
+            int age1 = 25;           this is called constructor , name same as class name
+         } */
+
+    public static void main(String[] args) {
+        int age = 28;
+        System.out.println(age);
+
+    }
+
+    //new method
+  /*  public void exam() {
+        int age = 50;       // local variable
+        System.out.println(age);
+
+    }*/
+
+   /* public void exam(int age1) {
+        int age1 = 10;       // local variable
+        System.out.println(age1);
+
+    } */
+
+}
--- src/main/java/org/matrix/sigma/pankaj/TestVariable.java
+++
@@ -1,8 +0,0 @@
-package org.matrix.sigma.pankaj;
-
-public class TestVariable {
-    public static void main(String[] args) {
-        int age = 28;
-        System.out.println(age);
-    }
-}
