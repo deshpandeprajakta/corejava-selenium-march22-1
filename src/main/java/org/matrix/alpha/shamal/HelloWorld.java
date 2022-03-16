44aaa306a68c7ae7e29fb834694ac8023fe33aa8
shamal18 <jadhavshamali1992>
2022-03-16 14:21:05 +0530

    New MAT-18-ShamalJ Branch created

--- src/main/java/org/matrix/alpha/shamal/HelloWorld.java
+++ src/main/java/org/matrix/alpha/shamal/HelloWorld.java
@@ -3,9 +3,17 @@ package org.matrix.alpha.shamal;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        int i=200;
+        int j=200;
+        if(i == j){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
         System.out.println("Hello, Welcome to Java!");
         System.out.println("Hello, Welcome to Java123!");
         System.out.println("Hello, Welcome to Java456!");
-        
+        System.out.println("New Brabch Created");
     }
 }
