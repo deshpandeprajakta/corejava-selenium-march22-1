488c8f83d8e673e138718c61f5816b692f19a74b
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-16 23:51:04 +0530

    added new local variable

--- src/main/java/org/matrix/sigma/pankaj/TestLocalVariable.java
+++ src/main/java/org/matrix/sigma/pankaj/TestLocalVariable.java
@@ -2,13 +2,14 @@ package org.matrix.sigma.pankaj;
 
 public class TestLocalVariable {
 
-  /*  {
-          int age2 = 24;   this is called blocks
-      }  */
+    {
+        int age2 = 24;   //this is called blocks
+    }
 
-    /*   TestLocalVariable() {
-            int age1 = 25;           this is called constructor , name same as class name
-         } */
+    TestLocalVariable() {
+        int age1 = 25;
+        System.out.println(age1);     // this is called constructor , name same as class name
+    }
 
     public static void main(String[] args) {
         int age = 28;
@@ -16,17 +17,17 @@ public class TestLocalVariable {
 
     }
 
-    //new method
-  /*  public void exam() {
+    //new method start
+    public void exam() {
         int age = 50;       // local variable
         System.out.println(age);
 
-    }*/
+    }
 
-   /* public void exam(int age1) {
-        int age1 = 10;       // local variable
-        System.out.println(age1);
+    public void exam(String str) {
+        int age = 10;       // local variable
+        System.out.println(age);
 
-    } */
+    }
 
 }
