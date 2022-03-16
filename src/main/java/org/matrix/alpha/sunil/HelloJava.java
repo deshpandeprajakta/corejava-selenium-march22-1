6d0bee6556e2c2b77f7acacefa012d5200a9158b
Sunil <sunilsonavane88@gmail.com>
2022-03-16 11:24:44 +0530

    Added Sout Statement

--- src/main/java/org/matrix/alpha/sunil/HelloJava.java
+++ src/main/java/org/matrix/alpha/sunil/HelloJava.java
@@ -3,5 +3,6 @@ package org.matrix.alpha.sunil;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Jawa");
+        System.out.println("Hello, Sunil!");
     }
 }
