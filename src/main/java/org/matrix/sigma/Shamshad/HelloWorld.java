810421f7f8d3607b64be30f8ed5079fdb96f3a9f
Shabbir Tamboli <shabbirtamboli64@gmail.com>
2022-03-16 11:12:58 +0530

    created HelloWorld.java

--- src/main/java/org/matrix/sigma/Shamshad/HelloWorld.java
+++ src/main/java/org/matrix/sigma/Shamshad/HelloWorld.java
@@ -3,7 +3,9 @@ package org.matrix.sigma.Shamshad;
 public class HelloWorld {
 
             public static void main(String[] args) {
-                System.out.println("Hello, shamshad!"); }
+            System.out.println("Hello software");
+            System.out.println("Hello selenium");
+            }
     }
 
 
