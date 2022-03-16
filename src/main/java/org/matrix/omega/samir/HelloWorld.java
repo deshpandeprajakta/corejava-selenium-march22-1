850c51c77c4969af36ce0b1bd478875d063f7453
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-16 20:23:53 +0530

    Added Extra Lines Of Codes

--- src/main/java/org/matrix/omega/samir/HelloWorld.java
+++ src/main/java/org/matrix/omega/samir/HelloWorld.java
@@ -2,26 +2,27 @@ package org.matrix.omega.samir;
 
 import java.util.Scanner;
 
-class HelloWorld{
+class HelloWorld extends InstanceVariables {
 
-    void employeeDetails(){
-        int empNumber=1,empsalary=5000;
-        Scanner sc= new Scanner(System.in);
+    void employeeDetails() {
+        int empNumber = 1, empsalary = 5000;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your number");
-        int no =sc.nextInt();
+        int no = sc.nextInt();
         System.out.println("hii");
-        String empName="samir",empLocation="pune";
-        System.out.println("Your entered Number "+ no +"empNumber=" + empNumber +" empsalary =   " + empsalary +" empName = " + empName +" empLocation= " + empLocation + "  " + empName.charAt(2) + " empName Lenght = " + empName.length());
+        String empName = "samir", empLocation = "pune";
+        System.out.println(super.companyName);
+        System.out.println("Your entered Number " + no + "empNumber=" + empNumber + " empsalary =   " + empsalary + " empName = " + empName + " empLocation= " + empLocation + "  " + empName.charAt(2) + " empName Lenght = " + empName.length());
     }
 
-    public static void main(String[] args){
-       HelloWorld HelloWorld1 = new HelloWorld();
-       HelloWorld1.employeeDetails();
-       for(int i=0;i<3;i++) {
+    public static void main(String[] args) {
+        HelloWorld HelloWorld1 = new HelloWorld();
+        HelloWorld1.employeeDetails();
+        for (int i = 0; i < 3; i++) {
 
 
-           System.out.println("You Are Right");
-       }
+            System.out.println("You Are Right");
+        }
 
     }
 }
--- src/main/java/org/matrix/omega/samir/InstanceVariables.java
+++ src/main/java/org/matrix/omega/samir/InstanceVariables.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InstanceVariables {
     int salary = 5000; // instance variable
-    static String companyName = "ABC"; // Static Variable
+    String companyName = "ABC"; // Static Variable
 
     void checkEvenOrOdd(int num) { //non static method
         if (num % 2 == 0) {
@@ -17,7 +17,7 @@ public class InstanceVariables {
     public static void main(String[] args) {
         InstanceVariables obj = new InstanceVariables();
         System.out.println("SALARY IS " + obj.salary);//calling using object
-        System.out.println("Company Name is " + companyName);//direct calling because it is a static variable
+        //System.out.println("Company Name is " + companyName);//direct calling because it is a static variable
         System.out.println("Enter the value of num");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
