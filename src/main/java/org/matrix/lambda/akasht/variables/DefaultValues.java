e8bff328e2f2b4d8e81e006c7af88eeecfe1b899
akash <Akash@7500>
2022-03-16 20:14:35 +0530

    Learning New Branches

---
+++ src/main/java/org/matrix/lambda/akasht/variables/DefaultValues.java
@@ -0,0 +1,10 @@
+package org.matrix.lambda.akasht.variables;
+
+public class DefaultValues {
+
+    public static void main(String[] args) {
+        System.out.println("Hello AkashT");
+        System.out.println("We are learning branching concept");
+        System.out.println("New sentence");
+    }
+}
