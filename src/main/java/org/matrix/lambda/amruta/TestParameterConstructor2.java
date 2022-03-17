a590b2238ff01f9bc682c76d34055e9c6ac30f03
amruta95327 <patilamruta16@gmail.com>
2022-03-17 18:01:48 +0530

    ParameterConstructor2 ex.

---
+++ src/main/java/org/matrix/lambda/amruta/TestParameterConstructor2.java
@@ -0,0 +1,14 @@
+package org.matrix.lambda.amruta;
+
+public class TestParameterConstructor2 {
+
+    String companyname;
+
+    private TestParameterConstructor2(String name){//this is parameter constructor
+        System.out.println("The company name is:"+name);
+    }
+
+    public static void main(String[] args) {
+        TestParameterConstructor2 obj=new TestParameterConstructor2("TCS");
+    }
+}
