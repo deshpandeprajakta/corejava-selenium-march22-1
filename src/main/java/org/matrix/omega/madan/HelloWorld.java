ed91d2cba2c43cc4f3d5824d42dfc891c543b57b
MadanPawar <pawarmadan638@gmail.com>
2022-03-17 12:37:01 +0530

    First program

--- src/main/java/org/matrix/omega/madan/HelloWorld.java
+++ src/main/java/org/matrix/omega/madan/HelloWorld.java
@@ -1,10 +1,9 @@
-package org.matrix.omega.madan;
-
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, Welcome to Java!");
+    public static void main(String [] args){
 
-        System.out.println();
+        System.out.println(  "Hello welcome to core java");
+
     }
+
 }
