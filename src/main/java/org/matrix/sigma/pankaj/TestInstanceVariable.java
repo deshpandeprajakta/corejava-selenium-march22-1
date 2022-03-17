4eb4e12619deb0f748e0f9315d4f97d1b096abb0
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-17 10:33:18 +0530

    Merge branch 'master' of https://github.com/shriniwasalle/corejava-selenium-march22

--- src/main/java/org/matrix/sigma/pankaj/TestInstanceVariable.java
+++ src/main/java/org/matrix/sigma/pankaj/TestInstanceVariable.java
@@ -5,12 +5,12 @@ public class TestInstanceVariable {
     public void display(){
         System.out.println(age);
     }
-
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
        // System.out.println(age); //non static variable can't use directly inside static method
 
     //memory allocation
     TestInstanceVariable obj = new TestInstanceVariable();
+        System.out.println(obj.age); //instance variable use by using object creation
 }
 }
 
