5f5ab0727261abff3aa61b407d04bfbc7bc8a535
Kamlesh Dandi <dandi8626@gmail.com>
2022-03-17 14:18:26 +0530

    Constructor various type examples

---
+++ src/main/java/org/matrix/alpha/kamlesh/constructors/ParameterizedConstructor.java
@@ -0,0 +1,17 @@
+package org.matrix.alpha.kamlesh.constructors;
+
+    public class ParameterizedConstructor {
+
+        public ParameterizedConstructor(int no,String name,double value) {
+            System.out.println(+no);
+            System.out.println("Kamlesh"+name);
+            System.out.println(value);
+        }
+
+        public static void main(String[] args) {
+            ParameterizedConstructor obj = new ParameterizedConstructor(1,"",7.5);
+        }
+
+    }
+
+
