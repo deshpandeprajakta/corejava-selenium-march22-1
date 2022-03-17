73615d6c1e9ad103ad49a21737baf8505f24280d
amruta95327 <patilamruta16@gmail.com>
2022-03-17 18:10:59 +0530

    DefaultConstructor ex.

---
+++ src/main/java/org/matrix/lambda/amruta/TestDefaultConstructor.java
@@ -0,0 +1,14 @@
+package org.matrix.lambda.amruta;
+
+public class TestDefaultConstructor {
+    int a;
+    boolean b;
+
+    public static void main(String[] args) {
+        TestDefaultConstructor obj=new TestDefaultConstructor();
+
+        System.out.println("a=" +obj.a);
+        System.out.println("b="+ obj.b);
+    }
+
+}
