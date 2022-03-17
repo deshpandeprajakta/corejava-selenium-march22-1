cfbe99664c33b77ceac2fa1392f635288a5fe7fa
MadanPawar <pawarmadan638@gmail.com>
2022-03-17 16:45:02 +0530

    First program

--- src/main/java/org/matrix/omega/madan/HelloWorld.java
+++ src/main/java/org/matrix/omega/madan/HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
     public static void main(String [] args){
 
-        System.out.println(  "Hello welcome to core java");
+
+        System.out.println("welcome to java world");
 
     }
-
 }
