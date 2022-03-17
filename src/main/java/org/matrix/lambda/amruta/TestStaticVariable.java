53c4f299d5946cc6b10f1f3f3110af9545160f29
amruta95327 <patilamruta16@gmail.com>
2022-03-17 17:11:28 +0530

    Staticvariable example

---
+++ src/main/java/org/matrix/lambda/amruta/TestStaticVariable.java
@@ -0,0 +1,18 @@
+package org.matrix.lambda.amruta;
+
+public class TestStaticVariable {
+
+    static int age;
+    static String name;
+    //This is static method
+    static  void display(){
+        System.out.println("Age is: "+age);
+        System.out.println("Name is: "+name);
+    }
+
+    public static void main(String[] args) {
+        age=20;
+        name="Amu";
+        display();
+    }
+}
