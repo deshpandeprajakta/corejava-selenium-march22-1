9c36f88b2e455e12112cdfa60cc3aea529c3ff35
amruta95327 <patilamruta16@gmail.com>
2022-03-17 17:35:18 +0530

    Non args Constructor 2

---
+++ src/main/java/org/matrix/lambda/amruta/TestConstructor2.java
@@ -0,0 +1,15 @@
+package org.matrix.lambda.amruta;
+
+public class TestConstructor2 {
+    int age;
+
+    public TestConstructor2(){ // no args constructor
+        age=30;
+        System.out.println("The Age:"+age);
+    }
+
+    public static void main(String[] args) {
+        TestConstructor2 obj=new TestConstructor2();
+
+    }
+}
