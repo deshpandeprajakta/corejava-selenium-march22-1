391257d9b220fab92dbb9966932819ddc1647ea8
amruta95327 <patilamruta16@gmail.com>
2022-03-17 17:29:25 +0530

    Non args Constructor

---
+++ src/main/java/org/matrix/lambda/amruta/TestConstructor.java
@@ -0,0 +1,16 @@
+package org.matrix.lambda.amruta;
+
+public class TestConstructor {
+    int empId;
+    String empName;
+
+    public TestConstructor(){
+        System.out.println("This is non-args constructor");
+
+    }
+
+    public static void main(String[] args) {
+        TestConstructor obj=new TestConstructor();
+
+    }
+}
