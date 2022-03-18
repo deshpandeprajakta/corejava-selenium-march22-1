204596ee3a92897631e7743b0fdc9872e187593a
kadambari <kadambari.belavate@gmail.com>
2022-03-18 08:01:30 -0700

    MAT-08-KadambariB-Practice constructer

--- src/main/java/org/matrix/omega/nayan/TestTwo.java
+++ src/main/java/org/matrix/omega/nayan/TestTwo.java
@@ -1,4 +1,4 @@
-package org.matrix.omega.nayan;
+/*package org.matrix.omega.nayan;
 import org.matrix.omega.nayan.TestOne;
 public class TestTwo {
 
@@ -19,3 +19,4 @@ public class TestTwo {
     }
 
 }
+*/
---
+++ src/main/java/org/matrix/sigma/kadambari/TestConstructor.java
@@ -0,0 +1,22 @@
+package org.matrix.sigma.kadambari;
+
+public class TestConstructor {
+    int x, y; // instance variable
+
+    TestConstructor(int a, int b)//
+    {
+
+
+        x = a;
+        y = b;
+        System.out.println("x: " + a);
+        System.out.println("y: " + b);
+        System.out.println("parameterize constructer ");
+    }
+
+    public static void main(String[] args) {
+        TestConstructor obj = new TestConstructor(10, 20);
+
+    }
+
+}
