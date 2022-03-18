9d71295e03077a197ed5796464579b42503c1f4e
Akash-0103 <akash.kudalkar001@gmail.com>
2022-03-18 23:24:25 +0530

    -Personal Information

---
+++ src/main/java/org/matrix/sigma/akash/MyInfo.java
@@ -0,0 +1,9 @@
+package org.matrix.sigma.akash;
+
+public class MyInfo {
+
+    public static void main(String[] args) {
+        System.out.println("My self Akash and i am from jejuri.");
+    }
+
+}
