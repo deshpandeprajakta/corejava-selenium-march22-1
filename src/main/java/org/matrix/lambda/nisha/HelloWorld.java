ca80fc8b241e87005c1f2860f973dc5d50726ca3
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-03-19 23:20:05 +0530

    Added new line

--- src/main/java/org/matrix/lambda/nisha/HelloWorld.java
+++ src/main/java/org/matrix/lambda/nisha/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Java!");
         System.out.println("New Branch Created");
+        System.out.println("New Branch Created");
     }
 }
