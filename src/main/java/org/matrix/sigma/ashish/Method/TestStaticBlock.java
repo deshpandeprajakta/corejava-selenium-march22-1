938660f5741fef4e34e90c32d40826951007e156
Ashishthopate <ashishthopte27@gmail.com>
2022-03-19 20:25:23 +0530

    Mat-30-AshishT Adding new Static Method Program

---
+++ src/main/java/org/matrix/sigma/ashish/Method/TestStaticBlock.java
@@ -0,0 +1,17 @@
+package org.matrix.sigma.ashish.Method;
+
+public class TestStaticBlock {
+
+    static{
+        System.out.println("I am in static block");
+    }
+
+    public TestStaticBlock(){
+        System.out.println("I am in constructor");
+    }
+
+    public static void main(String[] args) {
+        TestStaticBlock obj=new TestStaticBlock();
+        TestStaticBlock obj2=new TestStaticBlock();
+    }
+}
