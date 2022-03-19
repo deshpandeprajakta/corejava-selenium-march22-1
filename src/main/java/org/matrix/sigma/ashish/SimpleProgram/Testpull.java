c931475e8d6e2dddb3a4c85d990a977fca85ec90
Ashishthopate <ashishthopte27@gmail.com>
2022-03-19 22:15:16 +0530

    updated pull prog.

--- src/main/java/org/matrix/sigma/ashish/SimpleProgram/Testpull.java
+++ src/main/java/org/matrix/sigma/ashish/SimpleProgram/Testpull.java
@@ -3,5 +3,6 @@ package org.matrix.sigma.ashish.SimpleProgram;
 public class Testpull {
     public static void main(String[] args) {
         System.out.println("Ashish");
+        System.out.println("Thopate");
     }
 }
