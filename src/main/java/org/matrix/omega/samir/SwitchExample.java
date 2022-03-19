e2abb6b7e7ec5f193f76307010f00375a07d961a
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-19 20:57:55 +0530

    MAT-21-SamirS:Added Switch Example File

--- src/main/java/org/matrix/omega/samir/SwitchExample.java
+++ src/main/java/org/matrix/omega/samir/SwitchExample.java
@@ -1,4 +1,24 @@
 package org.matrix.omega.samir;
 
+import java.util.Scanner;
+
 public class SwitchExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the two numbers");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println("Enter the choice \n1. Addition\n 2. subtraction");
+        int ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("Addition is" + "" + (n + m));
+
+            case 2:
+
+                System.out.println("subtraction is" + "" + (n - m));
+
+
+        }
+    }
 }
