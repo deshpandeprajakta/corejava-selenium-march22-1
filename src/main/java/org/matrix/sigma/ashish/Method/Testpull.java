b3a1c04423b635578ae71ca10c374a57a06383bf
Ashishthopate <ashishthopte27@gmail.com>
2022-03-19 23:20:45 +0530

    updated pull prog.

--- src/main/java/org/matrix/sigma/ashish/Method/Testpull.java
+++ src/main/java/org/matrix/sigma/ashish/Method/Testpull.java
@@ -4,6 +4,6 @@ public class Testpull {
     public static void main(String[] args) {
         System.out.println("Ashish");
         System.out.println("Thopate");
-        System.out.println(" ");
+       // System.out.println(" ");
     }
 }
