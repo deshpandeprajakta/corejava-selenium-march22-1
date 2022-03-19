f50e2b07c08ac33b8deab0ccf0e8da6b7114e77c
Ashishthopate <ashishthopte27@gmail.com>
2022-03-19 20:14:12 +0530

    Mat-30-AshishT Adding new Instance Method Program

---
+++ src/main/java/org/matrix/sigma/ashish/Method/TestInstanceBlock.java
@@ -0,0 +1,29 @@
+package org.matrix.sigma.ashish.Method;
+
+public class TestInstanceBlock {
+    {
+        System.out.println("I am in Instance Block1");
+    }
+
+    {
+        System.out.println(" I am instance Block2");
+    }
+
+    public TestInstanceBlock(){
+        System.out.println("I am in Constructor");
+    }
+
+    public TestInstanceBlock(int age){
+        System.out.println(" I am in Parameter Passing Constructor"+age);
+    }
+
+    {
+        System.out.println(" I am in Instance Block 3");
+    }
+
+    public static void main(String[] args) {
+        TestInstanceBlock obj=new TestInstanceBlock();
+        System.out.println("--------------------------");
+        TestInstanceBlock obj2=new TestInstanceBlock(10);
+    }
+}
