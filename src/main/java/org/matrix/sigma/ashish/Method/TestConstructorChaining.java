de01e69ac1fe2d039dc1654d9e9f268c74183fb4
Ashishthopate <ashishthopte27@gmail.com>
2022-03-19 19:50:53 +0530

    Mat-30-AshishT Adding new Constructor Chaining Program

---
+++ src/main/java/org/matrix/sigma/ashish/Method/TestConstructorChaining.java
@@ -0,0 +1,27 @@
+package org.matrix.sigma.ashish.Method;
+
+public class TestConstructorChaining {
+
+    public TestConstructorChaining(){
+        this(30);
+        System.out.println("I am in No Arguments Constructor");
+    }
+
+    public TestConstructorChaining(int age){
+        this(30,"Ashish Thopate");
+        System.out.println(" I am in Single ParameterConstructor");
+        System.out.println("Employee age  "+age);
+        System.out.println("-------------------------------------------");
+    }
+
+    public TestConstructorChaining(int age, String name){
+        System.out.println(" I am in Double Parameter Construstor");
+        System.out.println("Employee Name  " +name);
+        System.out.println("Employee Age  " +age);
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        TestConstructorChaining obj=new TestConstructorChaining();
+    }
+}
