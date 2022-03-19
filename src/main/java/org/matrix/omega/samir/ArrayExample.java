f0ab08721176f874ef31a63906fd55f9ea38e005
Samir Shaikh <shaikhsameer30995@gmail.com>
2022-03-19 18:39:06 +0530

    MAT-21-SamirS:Added ArrayExample File

---
+++ src/main/java/org/matrix/omega/samir/ArrayExample.java
@@ -0,0 +1,23 @@
+package org.matrix.omega.samir;
+
+import java.util.Scanner;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[][] a = new int[10][10];
+      /*  Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 10 values");
+        for (int i = 0; i < 10; i++) {
+            a[i] = sc.nextInt();
+        }*/
+        for (int i = 0; i < 4; i++) {
+            System.out.println("1");
+            for (int j = 0; j < i; j++) {
+                System.out.println("2");
+
+            }
+            System.out.println("\n");
+            //System.out.println("The value present at   " + i + "  is  " + a[i]);
+        }
+    }
+}
