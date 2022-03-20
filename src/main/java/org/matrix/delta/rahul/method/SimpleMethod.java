350b65988f17b82b8b1fef1d4eefbe8f98514ab5
Rahul S <shendagerb100@gmail.com>
2022-03-20 22:34:13 +0530

    Simple program

--- src/main/java/org/matrix/delta/rahul/method/SimpleMethod.java
+++ src/main/java/org/matrix/delta/rahul/method/SimpleMethod.java
@@ -3,5 +3,6 @@ package org.matrix.delta.rahul.method;
 public class SimpleMethod {
     public static void main(String[] args) {
         System.out.println("I am in simple");
+        int a;
     }
 }
