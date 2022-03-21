50a51903f7594b136eb6c2947927efba3aa2cbdf
AkashNGupta <akashngupta2116@gmail.com>
2022-03-21 13:23:13 +0530

    Multi-level inheritance

---
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/Inheritance/Multilevel.java
@@ -0,0 +1,13 @@
+package org.matrix.lambda.akashgupta.javaconcepts.Inheritance;
+
+public class Multilevel extends SingleLevel {    //............SingleLevel extends Employee
+
+    public static void main(String[] args) {
+        SingleLevel obj =new SingleLevel();
+        obj.empEntry();
+        obj.displayList();
+        company="eInfochip";
+        employees[1].getData();
+    }
+
+}
--- src/main/java/org/matrix/lambda/akashgupta/javaconcepts/Inheritance/SingleLevel.java
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/Inheritance/SingleLevel.java
@@ -6,31 +6,26 @@ import java.util.Scanner;
 
 public class SingleLevel extends Employee {       // child_class extends parent_class
 
-     static Employee[] employees = new Employee[3];    //array of Employee-class object
+    static Employee[] employees = new Employee[3];    //array of Employee-class object
 
-    public   void empEntry(){
-        company="MicroSoft";                          //direct access to static variable of parent in child
-        for(int i =0;i<=2;i++){
+    public void empEntry() {
+        company = "MicroSoft";                          //direct access to static variable of parent in child
+        for (int i = 0; i <= 2; i++) {
 
-            employees[i]=createEmpObject();      // parent class static method (employees[] is an object of p-class)
+            employees[i] = createEmpObject();      // parent class static method (employees[] is an object of p-class)
             System.out.println("Enter Emp Data:");
             Scanner scan = new Scanner(System.in);
             System.out.println("Name:");
-            employees[i].empName=scan.nextLine();   //parent class instance (like obj.empName)
+            employees[i].empName = scan.nextLine();   //parent class instance (like obj.empName)
             System.out.println("EmpId:");
-            employees[i].empId=scan.nextInt();
+            employees[i].empId = scan.nextInt();
         }
     }
 
-    public void displayList(){
-        for (int i=0;i<=2;i++){
+    public void displayList() {
+        for (int i = 0; i <= 2; i++) {
             employees[i].getData();       // parent class method
         }
     }
 
-    public static void main(String[] args) {
-        SingleLevel obj =new SingleLevel();
-        obj.empEntry();
-        obj.displayList();
-    }
 }
