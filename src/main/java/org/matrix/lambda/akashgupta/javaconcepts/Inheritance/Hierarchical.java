2a06883c96da0e5a72bad6f1251fb23d392480f2
AkashNGupta <akashngupta2116@gmail.com>
2022-03-21 13:31:47 +0530

    Hierarchical inheritance

---
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/Inheritance/Hierarchical.java
@@ -0,0 +1,21 @@
+package org.matrix.lambda.akashgupta.javaconcepts.Inheritance;
+
+public class Hierarchical {
+    public static void main(String args[]) {
+        Cat c = new Cat();
+        c.meow();
+        c.eat();
+        //c.bark();                          //C.T.Error (cannot because Cat doesn't extends Dog)
+        Dog d = new Dog();
+        d.bark();
+    }
+}
+class Animal{
+    void eat(){System.out.println("eating...");}
+}
+class Dog extends Animal{
+    void bark(){System.out.println("barking...");}
+}
+class Cat extends Animal{
+    void meow(){System.out.println("meowing...");}
+}
