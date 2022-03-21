d97e24ea3158a5d82dc98644e18d31a880ac0e9d
NEHA <nehakonda214@mail.com>
2022-03-21 16:21:59 +0530

    Swapping of two numbers.

---
+++ src/main/java/org/matrix/omega/neha/SwapTwoNum.java
@@ -0,0 +1,17 @@
+package org.matrix.omega.neha;
+
+public class SwapTwoNum {
+    public static void main(String[] args) {
+        int first = 20;
+        int second = 50;
+        System.out.println("Before swap");
+        System.out.println("The first number is: " + first);
+        System.out.println("The second number is: " + second);
+        int temporary = first;     // First value stored in temporary.
+        first = second;            // Second value stored in first.
+        second = temporary;        // Temporary value stored in second.
+        System.out.println(" After swap");
+        System.out.println("The first number is: " + first);
+        System.out.println("The second number is: " + second);
+    }
+}
