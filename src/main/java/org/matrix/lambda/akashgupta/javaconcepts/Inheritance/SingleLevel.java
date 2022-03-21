e604e845584519cd6ee197310471e6fe4d24707f
AkashNGupta <akashngupta2116@gmail.com>
2022-03-21 12:48:11 +0530

    single level inheritance

---
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/Inheritance/SingleLevel.java
@@ -0,0 +1,36 @@
+package org.matrix.lambda.akashgupta.javaconcepts.Inheritance;
+
+import org.matrix.lambda.akashgupta.javaconcepts.corejava.classes.Employee;
+
+import java.util.Scanner;
+
+public class SingleLevel extends Employee {       // child_class extends parent_class
+
+     static Employee[] employees = new Employee[3];    //array of Employee-class object
+
+    public   void empEntry(){
+        company="MicroSoft";                          //direct access to static variable of parent in child
+        for(int i =0;i<=2;i++){
+
+            employees[i]=createEmpObject();      // parent class static method (employees[] is an object of p-class)
+            System.out.println("Enter Emp Data:");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Name:");
+            employees[i].empName=scan.nextLine();   //parent class instance (like obj.empName)
+            System.out.println("EmpId:");
+            employees[i].empId=scan.nextInt();
+        }
+    }
+
+    public void displayList(){
+        for (int i=0;i<=2;i++){
+            employees[i].getData();       // parent class method
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleLevel obj =new SingleLevel();
+        obj.empEntry();
+        obj.displayList();
+    }
+}
