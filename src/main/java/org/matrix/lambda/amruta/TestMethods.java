76384ef030c4c12febe4936140ae1eec6118adc1
amruta95327 <patilamruta16@gmail.com>
2022-03-22 10:54:59 +0530

    Method example

---
+++ src/main/java/org/matrix/lambda/amruta/TestMethods.java
@@ -0,0 +1,16 @@
+package org.matrix.lambda.amruta;
+
+public class TestMethods {
+    int a=100;
+    int b=200;
+
+    public void addition(int a,int b){
+        System.out.println(this.a+this.b);
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        TestMethods obj=new TestMethods();
+        obj.addition(5,10);
+    }
+
+}
