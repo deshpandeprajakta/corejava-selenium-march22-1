3c843489867c70228986715942d11be16fdcde40
amruta95327 <patilamruta16@gmail.com>
2022-03-22 18:22:52 +0530

    Methodchaining example

---
+++ src/main/java/org/matrix/lambda/amruta/TestMethodChaining.java
@@ -0,0 +1,20 @@
+package org.matrix.lambda.amruta;
+
+public class TestMethodChaining{
+
+  public void m1(){
+        System.out.println("This is 1st line");
+        m2();
+
+    }
+    public void m2(){
+        System.out.println("This is 2nd line");
+    }
+
+    public static void main(String[] args) {
+        TestMethodChaining obj = new TestMethodChaining();
+        obj.m1();
+
+
+    }
+}
