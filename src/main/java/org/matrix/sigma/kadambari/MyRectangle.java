6cc7f3ba7c0eb7e39c40c957630c1489e635d02c
kadambari <kadambari.belavate@gmail.com>
2022-03-23 01:47:12 -0700

    MAT-08-KadambariB-Constructors and this keyword

---
+++ src/main/java/org/matrix/sigma/kadambari/MyRectangle.java
@@ -0,0 +1,27 @@
+package org.matrix.sigma.kadambari;
+
+public class MyRectangle {
+    int lenght;
+    int breadth;
+
+    MyRectangle() {
+        lenght = 10;
+        breadth = 20;
+    }
+
+    MyRectangle(int lenght, int breadth) {
+        this.lenght = lenght;
+        this.breadth = breadth;
+    }
+
+    int area() {
+        return lenght * breadth;
+    }
+
+    public static void main(String[] args) {
+        MyRectangle obj = new MyRectangle();
+        MyRectangle obj2 = new MyRectangle(20, 25);
+        System.out.println("Area=" + obj2.area());
+    }
+
+}
---
+++ src/main/java/org/matrix/sigma/kadambari/TestString.java
@@ -0,0 +1,13 @@
+package org.matrix.sigma.kadambari;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        String st ="kadambari";//string literal (SCP-String Constanct Pool Memory)
+        String st1=new String ("shravi");//by using object creation -heap memory
+        String str3="Prashant";
+         str3= str3.concat("Belavate");
+        System.out.println(str3);
+    }
+
+}
