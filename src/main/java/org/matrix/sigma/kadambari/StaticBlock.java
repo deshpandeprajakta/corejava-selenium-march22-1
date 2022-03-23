6cb03dc6395ad932084cbc294e5437e4a6efc419
kadambari <kadambari.belavate@gmail.com>
2022-03-23 02:00:40 -0700

    MAT-08-KadambariB-Static Block

---
+++ src/main/java/org/matrix/sigma/kadambari/StaticBlock.java
@@ -0,0 +1,19 @@
+package org.matrix.sigma.kadambari;
+
+public class StaticBlock {
+    static int a;
+    static int b;
+    static{
+        System.out.println("Static block intialized.");
+        a=12; b=20;
+    }
+
+    public static void method(){
+        System.out.println("a =" +a);
+        System.out.println("b ="+b);
+    }
+
+    public static void main(String[] args) {
+        method();
+    }
+}
