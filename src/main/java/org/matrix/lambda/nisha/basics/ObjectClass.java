b1e5f5dddfe278ee8b3eb6bbc2afed4fc9e44080
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-03-24 19:13:22 +0530

    MAT-13-NishaV: Object Class

---
+++ src/main/java/org/matrix/lambda/nisha/basics/ObjectClass.java
@@ -0,0 +1,25 @@
+package org.matrix.lambda.nisha.basics;
+
+public class ObjectClass {
+
+    public static void main(String[] args) {
+
+    /*    Classes of Object
+        1. clone()
+        2. equals()
+        3. hashCode()
+        4. toString()
+        5. finalize()
+        6. getClass()
+        7. notify()
+        8. notifyAll()
+        9. a)wait()
+           b)wait(long timeout)
+           c)wait(long timeout, int nanos)*/
+    }
+}
+
+
+
+
+
