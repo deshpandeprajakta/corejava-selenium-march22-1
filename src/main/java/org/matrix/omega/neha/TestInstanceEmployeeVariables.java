87df31ab15dce7e18bcb791f4397780efad2b68d
NEHA <nehakonda214@mail.com>
2022-03-24 17:01:59 +0530

    Instance variable method calling from previous class

---
+++ src/main/java/org/matrix/omega/neha/TestInstanceEmployeeVariables.java
@@ -0,0 +1,25 @@
+package org.matrix.omega.neha;
+
+public class TestInstanceEmployeeVariables {
+    int empId = 11;
+    String empName = "Madhu";
+    static String companyname = "Microsoft";
+
+    void getdata() {
+        System.out.println(empId);
+        System.out.println(empName);
+        System.out.println(companyname);
+    }
+
+    public static void main(String[] args) {
+        TestInstanceEmployeeVariables obj = new TestInstanceEmployeeVariables();
+        obj.getdata();
+        System.out.println("----------");
+        
+        Employee emp = new Employee();  //calling from previous class
+        emp.display();
+        // System.out.println(emp.empId);  //calling from previous class
+        //System.out.println(emp.empName);//calling from previous class
+
+    }
+}
