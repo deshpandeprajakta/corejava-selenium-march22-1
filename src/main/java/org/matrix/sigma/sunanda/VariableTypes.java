3f2589ab7f241adf30caf9dda82963dc339b75ad
Sunanda1312 <sunandamathapati1994@gmail.com>
2022-03-24 20:20:30 +0530

    subtraction and addtion

---
+++ src/main/java/org/matrix/sigma/sunanda/VariableTypes.java
@@ -0,0 +1,12 @@
+package org.matrix.sigma.sunanda;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        int a=50;
+        int b=10;
+        int c=a+b;
+        int d=a-b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
