e137908b4ca48fb4ec70baaa06e12c2d70ffcfbb
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-24 10:26:32 +0530

    practise of local variable.

---
+++ src/main/java/org/matrix/sigma/pankaj/Variable/TestLocalVariable1.java
@@ -0,0 +1,12 @@
+package org.matrix.sigma.pankaj.Variable;
+
+public class TestLocalVariable1 {
+
+    //practice local variable
+    public static void main(String[] args) {
+        int a = 50;
+        System.out.println(a);
+    }
+
+}
+
