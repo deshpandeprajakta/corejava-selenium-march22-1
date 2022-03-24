9c57be4df54f2bdc9adb7d963e0a15c464d0f75d
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-03-24 19:42:33 +0530

    MAT-13-NishaV: Static Blocks 2 added

--- src/main/java/org/matrix/lambda/nisha/blocks/StaticBlocks2.java
+++ src/main/java/org/matrix/lambda/nisha/blocks/StaticBlocks2.java
@@ -1,5 +1,20 @@
 package org.matrix.lambda.nisha.blocks;
 
 public class StaticBlocks2 {
+    static {
+        System.out.println("Im in Static Block 1");         // Static Block
+    }
+
+    static {
+        System.out.println("Im in Static Block 2");         // Static Block
+    }
+
+    {
+        System.out.println("I'm in Instance Block 1");      // Instance block
+    }
+
+    StaticBlocks2() {
+        System.out.println("In Constructor");               // Constructor
+    }
 
 }
