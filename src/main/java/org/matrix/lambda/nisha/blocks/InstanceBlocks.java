932e0d11b9b38d9c348900663b5f82b43422908a
Deepakkumar <Deepakkumar.Vadisherla@bentley.com>
2022-03-24 19:20:29 +0530

    MAT-13-NishaV: Instance Block sout

--- src/main/java/org/matrix/lambda/nisha/blocks/InstanceBlocks.java
+++ src/main/java/org/matrix/lambda/nisha/blocks/InstanceBlocks.java
@@ -5,4 +5,17 @@ public class InstanceBlocks {
     // Instance Initializer block is used to initialize the instance data member.
     // It runs each time whenever an object of the class is created.
 
+    {
+        System.out.println("I'm in Instance Block 1");
+    }
+
+    public InstanceBlocks() {
+        System.out.println("Constructor  block");
+    }
+
+    {
+        System.out.println("I'm in Instance Block 2");
+    }
 }
+
+
