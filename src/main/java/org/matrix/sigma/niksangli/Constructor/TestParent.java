84c42deb1803cc432357f47d8a2aad5cde446058
Nikhilcoolka <Niknov1990>
2022-03-24 11:11:25 +0530

    - Java practice

---
+++ src/main/java/org/matrix/sigma/niksangli/Constructor/TestParent.java
@@ -0,0 +1,12 @@
+package org.matrix.sigma.niksangli.Constructor;
+
+public class TestParent
+{
+    TestParent()
+    {
+        System.out.println("This is test parent constructor");
+
+    }
+
+
+}
