f53f56c134eb527c75f8d3f7bfdadf0ee34d313e
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-24 11:08:58 +0530

    practise of Instance variable.

---
+++ src/main/java/org/matrix/sigma/pankaj/Variable/PractiseInstanceVariable.java
@@ -0,0 +1,15 @@
+package org.matrix.sigma.pankaj.Variable;
+
+public class PractiseInstanceVariable {
+    int age = 28;
+
+    public void present() {
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("i am pankaj my age is 28");
+    }
+}
+
+
