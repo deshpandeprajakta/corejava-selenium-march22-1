91e9782f65797783aad7198062c9ccd7c560ae97
Sunanda1312 <sunandamathapati1994@gmail.com>
2022-03-24 22:44:27 +0530

    Using Main Method Inside The Class

---
+++ src/main/java/org/matrix/sigma/sunanda/MainMethod.java
@@ -0,0 +1,11 @@
+package org.matrix.sigma.sunanda;
+public class MainMethod {
+    int id=10;
+    String name="sunanda";
+
+    public static void main(String []args){
+        MainMethod emp1=new MainMethod();
+        System.out.println(emp1.id);
+        System.out.println(emp1.name);
+    }
+}
