035d83e8844ce814185b412bf5102ff6a77e5077
amruta95327 <patilamruta16@gmail.com>
2022-03-24 10:22:31 +0530

    Methodwith ReturnType ex

---
+++ src/main/java/org/matrix/lambda/amruta/MethodWithReturnType.java
@@ -0,0 +1,24 @@
+package org.matrix.lambda.amruta;
+
+public class MethodWithReturnType{
+
+    public int addition(int a,int b){
+        int c = a+b;
+        return c;
+    }
+
+    public int multiplication(int a,int b){
+        int c=a*b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        MethodWithReturnType obj = new MethodWithReturnType();
+        int add = obj.addition(10, 20);
+        int multi=obj.multiplication(10,15);
+        System.out.println(add);
+        System.out.println(multi);
+
+    }
+    }
+
