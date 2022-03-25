973a0aee3a54c4ea9f041191d09e3f41d91141fd
akash <Akash@7500>
2022-03-25 18:11:48 +0530

    Employeetest

---
+++ src/main/java/org/matrix/lambda/akasht/corejava/Employee.java
@@ -0,0 +1,6 @@
+package org.matrix.lambda.akasht.corejava;
+
+public class Employee {
+    int empId = 100;
+    String empName = "Akash";
+}
--- src/main/java/org/matrix/lambda/akasht/corejava/variables/TestVariable.java
+++ src/main/java/org/matrix/lambda/akasht/corejava/variables/TestVariable.java
@@ -1,11 +1,22 @@
 package org.matrix.lambda.akasht.corejava.variables;
 
+import org.matrix.lambda.akasht.corejava.Employee;
+
 public class TestVariable {
 
-
-
-
     public static void main(String[] args) {
+        int age = 26;
+        System.out.println(age);
+    }
 
+    public void test(String str) {
+        int i = 10;
+        int age = 100;
+        System.out.println(i);
+        System.out.println(age);
+
+    //Object creation
+        Employee emp = new Employee();
+        System.out.println(emp);
     }
 }
