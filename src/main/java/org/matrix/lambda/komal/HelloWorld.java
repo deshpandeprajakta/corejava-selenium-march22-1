0b810f5e73cccfb51b755277b20ea17543aa2fc8
Komalmaneshinde <99737157+Komalmaneshinde@users.noreply.github.com>
2022-03-25 19:42:08 +0530

    Added new sout statement

--- src/main/java/org/matrix/lambda/komal/HelloWorld.java
+++ src/main/java/org/matrix/lambda/komal/HelloWorld.java
@@ -3,5 +3,6 @@ package org.matrix.lambda.komal;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to Java!");
+        System.out.println("Hello Selenium");
     }
 }
