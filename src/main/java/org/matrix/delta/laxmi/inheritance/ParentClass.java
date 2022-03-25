a5c89f1cb78c0d0d5c96766eb00a8aa6d47af14b
laxmikurhade <laxmikurhade09@gmail.com>
2022-03-25 15:18:53 +0530

    Parent Class for inheritance-files added in MAT-16-LaxmiK

---
+++ src/main/java/org/matrix/delta/laxmi/inheritance/ParentClass.java
@@ -0,0 +1,12 @@
+package org.matrix.delta.laxmi.inheritance;
+
+public class ParentClass {
+    public void m1(){
+        System.out.println("This is Method 1 of Parent-Class");
+    }
+    public static void main(String[] args) {
+        ParentClass pc=new ParentClass();
+        pc.m1();
+    }
+}
+
