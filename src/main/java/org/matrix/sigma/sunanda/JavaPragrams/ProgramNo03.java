36f11f65ebca33e119c12f73b2f3e2e551805aa4
Sunanda1312 <sunandamathapati1994@gmail.com>
2022-03-25 20:52:03 +0530

    Program to count words in sentence

--- src/main/java/org/matrix/sigma/sunanda/JavaPragrams/ProgramNo03.java
+++ src/main/java/org/matrix/sigma/sunanda/JavaPragrams/ProgramNo03.java
@@ -1,20 +1,18 @@
 package org.matrix.sigma.sunanda.JavaPragrams;
 
 public class ProgramNo03 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String str = "My Name Is Sunanda Mathapati";
         System.out.println(str);
         int count = 0;
-        for (int i = 1; i < (str.length() - 1); i++) {
-
+        for (int i = 0; i < (str.length() - 1); i++)   //Counts all the spaces present in the string
+        {
             if (str.charAt(i) == ' ' && str.charAt(i + 1) != ' ') {
                 count++;
-                System.out.println("No of words in a String: " + count);
+
             }
-
         }
+        System.out.println("No of words in a String: " + count);
     }
-
-
-
 }
+
