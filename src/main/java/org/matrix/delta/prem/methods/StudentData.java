2d1313cf26a74235f78e33ba00c99b2f22742b15
Premp <prem74729@gmail.com>
2022-03-25 20:21:35 +0530

    MAT-36-PremP-Methods practice

--- src/main/java/org/matrix/delta/prem/methods/StudentData.java
+++ src/main/java/org/matrix/delta/prem/methods/StudentData.java
@@ -5,19 +5,19 @@ public class StudentData {
         String name="Yash";
         int age = 28;
         String branch ="Civil";
+        public static String College="SDITS";
+
 
         public void getData(){
                 System.out.println("College Id is= "+clgId);
                 System.out.println("Name of Student is= "+name);
                 System.out.println("Age of Student is= "+age);
                 System.out.println("Branch of Student is= "+branch);
+                System.out.println("College is : " + StudentData.College);
 
                 }
 
-        public static void main(String[] args) {
-                StudentData obj = new StudentData();
-                obj.getData();
-        }
+
 
                 }
 
