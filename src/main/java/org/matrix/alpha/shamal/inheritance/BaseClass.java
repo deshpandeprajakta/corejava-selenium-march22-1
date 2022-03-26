7c2e3e1713ce12b9b2b4a1ddd97e94f3e7bbc01f
shamal18 <jadhavshamali1992>
2022-03-26 15:57:18 +0530

    Keep Practicing on Inheritance Concept with Program

---
+++ src/main/java/org/matrix/alpha/shamal/inheritance/BaseClass.java
@@ -0,0 +1,11 @@
+package org.matrix.alpha.shamal.inheritance;
+
+public class BaseClass {
+
+    int x = 20;
+    public void msg(){
+        System.out.println("Hi i am Base Class Method");
+    }
+}
+
+
