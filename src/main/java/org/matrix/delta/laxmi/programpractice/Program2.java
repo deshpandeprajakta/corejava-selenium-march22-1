588911ed130ccd8914cfda5af2847f1e2454a0a7
laxmikurhade <laxmikurhade09@gmail.com>
2022-03-27 17:03:28 +0530

    Updated Reverse String in sentence Program2-files added in MAT-16-LaxmiK

--- src/main/java/org/matrix/delta/laxmi/programpractice/Program2.java
+++ src/main/java/org/matrix/delta/laxmi/programpractice/Program2.java
@@ -3,7 +3,7 @@ package org.matrix.delta.laxmi.programpractice;
 //Reverse string in sentence
 public class Program2 {
     public static void main(String[] args) {
-        String str = new String("My name is Laxmi");
+        String str = "My name is Laxmi";
         String[] strA = str.split(" ");
         for (int i = strA.length - 1; i >= 0; i--) {
             System.out.println(strA[i]);
