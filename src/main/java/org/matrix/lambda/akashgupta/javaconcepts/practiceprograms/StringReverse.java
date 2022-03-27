5dfd15b5c36ba708c1194dace2ac411dc1738a6b
AkashNGupta <akashngupta2116@gmail.com>
2022-03-27 21:18:07 +0530

    String reverse (reformatted) don't understand following warning Explicit type argument Character can be replaced with <>57

--- src/main/java/org/matrix/lambda/akashgupta/javaconcepts/practiceprograms/StringReverse.java
+++ src/main/java/org/matrix/lambda/akashgupta/javaconcepts/practiceprograms/StringReverse.java
@@ -8,15 +8,15 @@ import java.util.ListIterator;
 public class StringReverse {
     String str = "1MIRROR_IMAGE || EGAMI_RORRIM2";
 
-    public StringBuilder usingStringBuilderClass(String revstr){   //using StringBuilder(built-in java class)
+    public StringBuilder usingStringBuilderClass(String revstr) {   //using StringBuilder(built-in java class)
         StringBuilder obj = new StringBuilder(revstr);
         System.out.println("................using StringBuilder Class...............");
         return obj.reverse();                           //using  reverse(); method
     }
 
-    public void usingStringClassMethod1(String revstr){
-        int j=revstr.length()-1,i=0;
-        char[] chars = revstr.toCharArray(),buffer = new char[j+1];//using String class method to convert (String --> char[])
+    public void usingStringClassMethod1(String revstr) {
+        int j = revstr.length() - 1, i = 0;
+        char[] chars = revstr.toCharArray(), buffer = new char[j + 1];//using String class method to convert (String --> char[])
         System.out.println("................using toCharArray() method(String Class)...............");
         //System.out.println(chars);             //just converts String --> char[] doesn't reverse
         /*for(char c:chars){
@@ -24,45 +24,45 @@ public class StringReverse {
             i++;
             j--;
         }*/        //use buffer::overwriting issue, works fine for palindrome
-        while(j>=0){
-            buffer[i]=chars[j];
+        while (j >= 0) {
+            buffer[i] = chars[j];
             i++;
             j--;
         }
         System.out.println(buffer);
     }
 
-    public void usingStringClassMethod2(String revstr){
-        int j=revstr.length();
+    public void usingStringClassMethod2(String revstr) {
+        int j = revstr.length();
         char[] buffer = new char[j];
-        j=0;
+        j = 0;
         System.out.println("................using charAt(int index) method(String Class)...............");
-        for(int i=(revstr.length()-1);i>=0;i--){
-            buffer[j]=revstr.charAt(i);//using String Class method  to access directly at index....charAt(int index)
-            j+=1;
+        for (int i = (revstr.length() - 1); i >= 0; i--) {
+            buffer[j] = revstr.charAt(i);//using String Class method  to access directly at index....charAt(int index)
+            j += 1;
         }
         System.out.println(buffer);
     }
 
-    public StringBuffer usingStringBufferClass(String revstr){   //using StringBuffer (built-in java Class)
+    public StringBuffer usingStringBufferClass(String revstr) {   //using StringBuffer (built-in java Class)
         StringBuffer obj = new StringBuffer(revstr);
         System.out.println("................using StringBuffer Class...............");
         return obj.reverse();
     }
 
-    public void usingCollectionsClass(String revstr){       //revisit after interface theory
-        char[] chars =revstr.toCharArray();
+    public void usingCollectionsClass(String revstr) {       //revisit after interface theory
+        char[] chars = revstr.toCharArray();
         char[] buffer = new char[revstr.length()];
-        int i=0;
+        int i = 0;
         List<Character> list = new ArrayList<Character>();     //creating List(interface) type using ArrayList class
-        for(char ch:chars){    //'for-each':loop through each element for given array[]....for(Type var: arrayName){}
+        for (char ch : chars) {    //'for-each':loop through each element for given array[]....for(Type var: arrayName){}
             list.add(ch);
         }
         Collections.reverse(list);                             //collections method which accepts List type
         ListIterator<Character> listIt = list.listIterator();  //interface
         System.out.println("................using ListIterator...............");
-        while(listIt.hasNext()){
-            buffer[i]=listIt.next();
+        while (listIt.hasNext()) {
+            buffer[i] = listIt.next();
             i++;
         }
         System.out.println(buffer);
