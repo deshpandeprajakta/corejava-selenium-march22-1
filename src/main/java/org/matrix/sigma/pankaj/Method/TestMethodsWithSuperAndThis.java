4957ad63f62dc9aa3c8532a4d799e4af71c7658e
Ashishthopate <ashishthopte27@gmail.com>
2022-03-28 21:04:10 +0530

    updated methods

--- src/main/java/org/matrix/sigma/pankaj/Method/TestMethodsWithSuperAndThis.java
+++ src/main/java/org/matrix/sigma/pankaj/Method/TestMethodsWithSuperAndThis.java
@@ -4,13 +4,14 @@ import org.matrix.sigma.pankaj.Employee;
 
 public class TestMethodsWithSuperAndThis extends Employee {
 
-    public void s1() {
+    public void s1()
+    {
         System.out.println("i am in TestMethodsWithSuperAndThis :: s1 method ");
     }
 
     public void s2() {
         this.s1();
-        super.s1();
+      //  super.s1();
 
 
     }
