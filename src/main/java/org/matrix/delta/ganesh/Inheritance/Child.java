f77dce57e7c1f59cba97cec84fe8b3e34984d2c6
ganeshpirale <ganesh.pirale@gmail.com>
2022-03-29 16:36:50 +0530

    MAT-05-GaneshP: Method overriding program

--- src/main/java/org/matrix/delta/ganesh/Inheritance/Child.java
+++ src/main/java/org/matrix/delta/ganesh/Inheritance/Child.java
@@ -4,12 +4,14 @@ public class Child extends Parent {
 
     public void myChild(){
         System.out.println("In Child");
+        System.out.println("Method overriding in Child");
     }
 
     public static void main(String[] args) {
         Parent.myParent();
 
         Child child = new Child();
+        child.myChild(10);
 //        child.myParent();
         child.myChild();
 
--- src/main/java/org/matrix/delta/ganesh/Inheritance/Parent.java
+++ src/main/java/org/matrix/delta/ganesh/Inheritance/Parent.java
@@ -6,6 +6,14 @@ public class Parent {
         System.out.println("In Parent");
     }
 
+    public void myChild(){
+        System.out.println("Method overriding in Parent");
+    }
+
+    public void myChild(int age){
+        System.out.println("Method overriding with age parameter: "+age);
+    }
+
     public static void main(String[] args) {
             Parent parent = new Parent();
 //            parent.myParent();
