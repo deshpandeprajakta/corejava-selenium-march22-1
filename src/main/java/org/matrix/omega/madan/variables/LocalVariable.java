2ba287053c4e9f914187cc3cea29235bacb05708
MadanPawar <pawarmadan638@gmail.com>
2022-03-30 01:23:42 +0530

    Local and Instance Variable

--- src/main/java/org/matrix/omega/madan/variables/LocalVariable.java
+++ src/main/java/org/matrix/omega/madan/variables/LocalVariable.java
@@ -5,27 +5,27 @@ public class LocalVariable  {
 
     LocalVariable() {
 
-        int i =10;
-        double j=2.00;
+        int age = 10;
+        double code =2.00;
         String str1 = "JavaPrograming";
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println(age);
+        System.out.println(code);
         System.out.println(str1);
     }
      public static void  main (String [] args){
 
         LocalVariable l1 = new LocalVariable();
         l1.test();
-        int i = 20;
-        double d = 30.00;
-        System.out.println(i);
-        System.out.println(d);
+        int value= 20;
+        double code = 30.00;
+        System.out.println(value);
+        System.out.println(code);
 
      }
 
      public void test() {
 
-        int k = 30;
+        int age = 30;
         String str2 = "java";
         System.out.println(str2);
 
