8d2b0ea84b7fc4f8c6c21640311d4c1439904570
jagukudale <jagannath.kudale@gmail.com>
2022-03-30 16:47:24 +0530

    MAT-37-Jagannath : Method Overriding Concept

---
+++ src/main/java/org/matrix/omega/jagannath/corejava/Polymorphism/Overloading/Overriding/TestOverrideParent.java
@@ -0,0 +1,8 @@
+package org.matrix.omega.jagannath.corejava.Polymorphism.Overloading.Overriding;
+
+public class TestOverrideParent {
+    public void method1()
+    {
+        System.out.println("I am in parent Method1");
+    }
+}
