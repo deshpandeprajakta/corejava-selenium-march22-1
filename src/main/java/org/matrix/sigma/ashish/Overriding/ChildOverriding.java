a45c5b49c0387237f1e1f751aabe14855cc2bc77
Ashishthopate <ashishthopte27@gmail.com>
2022-03-30 08:40:09 +0530

    Added 2 programs of overriding

---
+++ src/main/java/org/matrix/sigma/ashish/Overriding/ChildOverriding.java
@@ -0,0 +1,35 @@
+package org.matrix.sigma.ashish.Overriding;
+
+public class ChildOverriding extends ParentOverriding {
+    
+    public void m1(){
+        System.out.println("I am in Child Overriding");
+    }
+
+    public void  m1(int a){
+        System.out.println("I am in Child m1[int] method");
+    }
+
+    public void m1(String str){
+        System.out.println("I am in Child m1[string] method");
+    }
+
+    public static void main(String[] args) {
+
+        ParentOverriding obj=new ParentOverriding();//Parent Ref and Parent Object
+        obj.m1(20);
+        obj.m1();
+        System.out.println(" ");
+
+        ChildOverriding obj1=new ChildOverriding();//Child Ref and Child obj
+        obj1.m1();
+        System.out.println(" ");
+
+        ParentOverriding p=new ChildOverriding();//Parent Ref and Child obj
+        p.m1();//actual overriding concept parent ref but child obj
+        p.m1(30);
+        p.m1("Ashish");
+
+        }
+    }
+
---
+++ src/main/java/org/matrix/sigma/ashish/Overriding/ParentOverriding.java
@@ -0,0 +1,16 @@
+package org.matrix.sigma.ashish.Overriding;
+
+public class ParentOverriding {
+
+    public void m1(){
+        System.out.println("I am in Parent Overriding");
+    }
+
+    public void m1(int a){
+        System.out.println("I am in m1 [int]  method");
+    }
+
+    public void m1(String str){
+        System.out.println(" I am in m1[string ] method");
+    }
+}
