c77054de3034a6bbe80f789c48d4a932f381ccaa
Ashishthopate <ashishthopte27@gmail.com>
2022-03-30 08:59:47 +0530

    Added 2 programs of overriding

---
+++ src/main/java/org/matrix/sigma/ashish/Overriding/NewChild.java
@@ -0,0 +1,15 @@
+package org.matrix.sigma.ashish.Overriding;
+
+public class NewChild extends NewParent {
+
+    public String m1(){
+        System.out.println(" I am in Child m1 method");
+        return "Ash";
+    }
+
+    public static void main(String[] args) {
+        NewParent p=new NewChild();
+        p.m1();//diff return type-covariant returntype
+
+    }
+}
---
+++ src/main/java/org/matrix/sigma/ashish/Overriding/NewParent.java
@@ -0,0 +1,9 @@
+package org.matrix.sigma.ashish.Overriding;
+
+public class NewParent {
+
+    public Object m1(){
+        System.out.println(" I am in parent m1 method");
+        return 1;
+    }
+}
