b0009d599a421e7fcaf51310a0805df9a82e6d4d
Ashishthopate <ashishthopte27@gmail.com>
2022-03-30 07:54:07 +0530

    updated in  Polimorphsam program

--- src/main/java/org/matrix/sigma/ashish/Polimorphisam/Testoverloading.java
+++ src/main/java/org/matrix/sigma/ashish/Polimorphisam/Testoverloading.java
@@ -19,7 +19,6 @@ public class Testoverloading {
         System.out.println(a);
         System.out.println("I am in Int passing parameter");
     }
-
     public void m2(int a,String str){
         System.out.println("integer   "+a);
         System.out.println("String   "+str);
@@ -30,7 +29,15 @@ public class Testoverloading {
         System.out.println(b);
     }
 
-    public static void main(String[] args) {
+        public static void m3(String str){
+            System.out.println("In static m3 string method   "+str);
+        }
+
+        public static void m3(Boolean b){
+            System.out.println("In Static m3 boolean method  "+b);
+        }
+
+       public static void main(String[] args) {
 
         Testoverloading obj=new Testoverloading();
           obj.add(10,20);
@@ -44,6 +51,16 @@ public class Testoverloading {
         obj.m2(100,200);
         System.out.println("-------------------------------------------");
         obj.m2(300,"Ashish");
+           System.out.println(" ");
+        obj.m3("ashish");
+        obj.m3(true);
+           System.out.println(" ");
+        int[] ar={10,20,30};
+        main(ar);
 
+    }
+
+    public static void main(int[] a){
+        System.out.println("In overloaded main method");
     }
 }
