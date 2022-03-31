51323f8dafc5f87102db7f10bdae276993f74cc7
shriniwasalle <shriniwasalle4@gmail.com>
2022-03-31 22:25:23 +0530

    - interface concept

---
+++ src/main/java/org/matrix/shriniwas/corejava/abstraction/interfaceconcept/TestInterface2.java
@@ -0,0 +1,9 @@
+package org.matrix.shriniwas.corejava.abstraction.interfaceconcept;
+
+public interface TestInterface2 {
+
+    public void add();
+    public void multiply();
+    String getText();
+
+}
