cebdce52c769807361d75f06902b23d443dd6a2e
kurhadepankaj <kurhadepankaj93@gmail.com>
2022-03-31 12:18:57 -0500

    practise overloading concept

--- src/main/java/org/matrix/sigma/pankaj/corejava/oopsconcept/polymorphism/overloading/TestOverloading1.java
+++ src/main/java/org/matrix/sigma/pankaj/corejava/oopsconcept/polymorphism/overloading/TestOverloading1.java
@@ -11,11 +11,23 @@ public class TestOverloading1 {
         System.out.println(a + b + c);
 
     }
+    public void p1(String str){
+        System.out.println("i am in p1 method");
+    }
+
+    public void p2(int a ,String str){
+        System.out.println("i am in p2 method");
+    }
+    public void p2(String str, int a){
+        System.out.println("i am in p2 method");
+    }
 
     public static void main(String[] args) {
         TestOverloading1 obj = new TestOverloading1();
         obj.add(50, 60);
         obj.add(50, 60, 70);
+        obj.p1("pankaj");
+        obj.p2(10,"pk");
 
     }
 
