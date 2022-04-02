8bf206f99cfda751e09adc45cc341dc01860891b
laxmikurhade <laxmikurhade09@gmail.com>
2022-04-02 22:07:10 +0530

    Test Constructor Program-files added in MAT-16-LaxmiK

---
+++ src/main/java/org/matrix/delta/laxmi/TestConstructor.java
@@ -0,0 +1,13 @@
+package org.matrix.delta.laxmi;
+public class TestConstructor {
+  public  void TestConstructor(){
+        System.out.println("Amsterdam");
+  }
+    TestConstructor(){
+        System.out.println("Antarctica");
+    }
+
+    public static void main(String[] args) {
+          new TestConstructor();
+    }
+}
