948895288e2552b714ac702cd6569e8f440b7bf7
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-03 17:34:33 +0530

    Add another one file

---
+++ src/main/java/org/matrix/sigma/prajakta/TestInstanceVariable.java
@@ -0,0 +1,20 @@
+package org.matrix.sigma.prajakta;
+
+public class TestInstanceVariable {
+
+    int i=10;
+
+    TestInstanceVariable(){
+        System.out.println("This is instance variable:-"+i);
+    }
+
+    public void getInstance(){
+        System.out.println("You are in method :-"+i); //instance variable can also fetch in method.
+    }
+    public static void main(String[] args) {
+        TestInstanceVariable obj=new TestInstanceVariable();
+        obj.getInstance();
+
+        //But not fetch in static method.
+    }
+}
