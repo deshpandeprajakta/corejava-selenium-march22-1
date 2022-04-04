8b18aa92fcc7b62305c6ac37139b784327fd75a2
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-04 14:25:08 +0530

    Updated some code

--- src/main/java/org/matrix/sigma/prajakta/Demo.java
+++ src/main/java/org/matrix/sigma/prajakta/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 
     public static void main(String[] args) {
         // this is demo program to understand how to push the record from Intellij.
+        System.out.println("This is demo program");
     }
 }
