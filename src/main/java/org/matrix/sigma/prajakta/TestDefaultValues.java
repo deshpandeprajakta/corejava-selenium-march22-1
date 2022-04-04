3e134ff10c33f96acc5de0538a0b25ff3e389d9c
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-04 16:27:44 +0530

    Add some code to TestDefaultValues

--- src/main/java/org/matrix/sigma/prajakta/TestDefaultValues.java
+++ src/main/java/org/matrix/sigma/prajakta/TestDefaultValues.java
@@ -1,4 +1,32 @@
 package org.matrix.sigma.prajakta;
 
+class Employee {
+
+}
+
 public class TestDefaultValues {
+
+    static int i;
+    static long l;
+    static short s;
+    static double d;
+    static char c;
+    static boolean b;
+    static float f;
+    static byte bb;
+    static Employee emp;
+
+    public static void main(String[] args) {
+        System.out.println("Data types default values:");
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(bb);
+        System.out.println(emp);
+    }
+
 }
