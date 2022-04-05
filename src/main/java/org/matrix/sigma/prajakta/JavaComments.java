3c5847cbc6354e452ce3079cfeb91a7176fda18a
deshpandeprajakta <deshpande.pa02@gmail.com>
2022-04-05 11:53:58 +0530

    Add one more file

--- src/main/java/org/matrix/sigma/prajakta/JavaComments.java
+++ src/main/java/org/matrix/sigma/prajakta/JavaComments.java
@@ -1,19 +1,21 @@
 package org.matrix.sigma.prajakta;
 
 public class JavaComments {
-    public static void main(String[] args) {
 
-        // This is single line comment.
+    // This is single line comment.
+
         /* This is multiline comment.
          --------
          --------
          */
 
-        /**
-         * this is documented comment
-         *
-         *
-         */
+    /**
+     * this is documented comment
+     *
+     */
+
+    public static void main(String[] args) {
+        System.out.println("This is comments in java");
 
     }
 }
